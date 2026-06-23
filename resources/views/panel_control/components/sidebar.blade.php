83caf84b7475bdf1e006050649e6aee47082794a
dzaki_omdb_2b <muhammaddzaki0909@gmail.com>
2026-06-23 15:26:02 +0700

    perubahan sidebar

--- resources/views/panel_control/components/sidebar.blade.php
+++ resources/views/panel_control/components/sidebar.blade.php
@@ -11,7 +11,7 @@
       <span class="app-brand-logo demo">
         <i class="bx bx-book-open fs-3 text-primary"></i>
       </span>
-      <span class="app-brand-text demo menu-text fw-bolder ms-2">Perpustakaan</span>
+      <span class="app-brand-text demo menu-text fw-bolder ms-2">Mini Library</span>
     </a>
     <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
       <i class="bx bx-chevron-left bx-sm align-middle"></i>

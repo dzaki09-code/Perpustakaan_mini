f1a1d9bf017992c19d43cda3833cdfe20b564f6e
Rizkiemhade <rizkiemhade300804@gmail.com>
2026-07-14 23:12:29 +0700

    Fix foto profil di header pakai S3 temporary URL

--- resources/views/panel_control/components/header.blade.php
+++ resources/views/panel_control/components/header.blade.php
@@ -1,7 +1,7 @@
 @php
     $authUser = Auth::user();
-    $profilePhotoUrl = $authUser && $authUser->profile_photo_path
-        ? Storage::disk('public')->url($authUser->profile_photo_path)
+   $profilePhotoUrl = $authUser && $authUser->profile_photo_path 
+         ? Storage::disk('s3')->temporaryUrl($authUser->profile_photo_path, now()->addMinutes(30))
         : null;
 @endphp
 
